fb7d0f478b08b65c1b6f65d7eb7911ad31937ada
DeepakKumarV-2004 <deepakvel2004@gmail.com>
2025-08-08 16:18:44 +0530

    springcore

---
+++ spring-core-1/src/main/java/com/tnsif/spring_core_1/Bike.java
@@ -0,0 +1,10 @@
+package com.tnsif.spring_core_1;
+
+public class Bike implements Vehical{
+	
+	@Override
+	public void ride() {
+		System.out.println("Bike is rideing......");
+	}
+
+}
---
+++ spring-core-1/src/main/java/com/tnsif/spring_core_1/Car.java
@@ -0,0 +1,10 @@
+package com.tnsif.spring_core_1;
+
+public class Car implements Vehical{
+	
+	@Override
+	public void ride() {
+		System.out.println("caar is riding");
+	}
+
+}
